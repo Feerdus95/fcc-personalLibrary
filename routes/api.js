6379fa38eab516635eeb18ab05f9655040287da1
Feerdus95 <ferdus95.fv@gmail.com>
2025-02-26 18:50:06 -0300

    fix: api.js v4

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,10 @@
-/*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
-
 'use strict';
 
 const Book = require('../models/book');
 
 module.exports = function (app) {
-
   app.route('/api/books')
-    .get(async function (req, res){
+    .get(async function (req, res) {
       try {
         const books = await Book.find();
         res.json(books.map(book => ({
@@ -23,34 +14,29 @@ module.exports = function (app) {
         })));
       } catch (err) {
         console.error(err);
-        res.status(500).send('Error retrieving books');
+        res.status(500).json([]);
       }
     })
-    
-    .post(async function (req, res){
-      let title = req.body.title;
+
+    .post(async function (req, res) {
+      const title = req.body.title;
       if (!title) {
         return res.status(400).send('missing required field title');
       }
       try {
-        const newBook = new Book({ 
-          title,
-          comments: [],
-          commentcount: 0
-        });
+        const newBook = new Book({ title });
         const savedBook = await newBook.save();
-        res.json({ 
-          _id: savedBook._id, 
-          title: savedBook.title 
+        res.json({
+          _id: savedBook._id,
+          title: savedBook.title
         });
       } catch (err) {
         console.error(err);
         res.status(500).send('Error adding book');
       }
     })
-    
-    .delete(async function(req, res){
-      //if successful response will be 'complete delete successful'
+
+    .delete(async function (req, res) {
       try {
         await Book.deleteMany({});
         res.send('complete delete successful');
@@ -60,9 +46,8 @@ module.exports = function (app) {
     });
 
   app.route('/api/books/:id')
-    .get(async function (req, res){
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    .get(async function (req, res) {
+      const bookid = req.params.id;
       try {
         if (!bookid.match(/^[0-9a-fA-F]{24}$/)) {
           return res.status(404).send('no book exists');
@@ -80,10 +65,10 @@ module.exports = function (app) {
         res.status(404).send('no book exists');
       }
     })
-    
-    .post(async function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
+
+    .post(async function (req, res) {
+      const bookid = req.params.id;
+      const comment = req.body.comment;
       if (!comment) {
         return res.status(400).send('missing required field comment');
       }
@@ -93,7 +78,6 @@ module.exports = function (app) {
           return res.status(404).send('no book exists');
         }
         book.comments.push(comment);
-        book.commentcount = book.comments.length;
         const updatedBook = await book.save();
         res.json({
           _id: updatedBook._id,
@@ -104,10 +88,9 @@ module.exports = function (app) {
         res.status(404).send('no book exists');
       }
     })
-    
-    .delete(async function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+
+    .delete(async function (req, res) {
+      const bookid = req.params.id;
       try {
         if (!bookid.match(/^[0-9a-fA-F]{24}$/)) {
           return res.status(404).send('no book exists');
@@ -121,5 +104,4 @@ module.exports = function (app) {
         res.status(404).send('no book exists');
       }
     });
-  
 };
